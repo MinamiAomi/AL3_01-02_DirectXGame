1352292db6448f218dfdadb3d8b4cd1c3938f788
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-04-17 21:45:39 +0900

    02_01

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,10 +1,6 @@
 ﻿#include "Player.h"
 #include <cassert>
 #include "Input.h"
-#include "MathUtils.h"
-#include "ImGuiManager.h"
-
-static float imguiPos[3] = {};
 
 void Player::Initalize(std::shared_ptr<Model> model, uint32_t texHandle) { 
 	assert(model);	
@@ -15,42 +11,9 @@ void Player::Initalize(std::shared_ptr<Model> model, uint32_t texHandle) {
 }
 
 void Player::Update() { 
-	auto input = Input::GetInstance();
 	
-	// キャラクターの移動ベクトル
-	Vector3 move = {0, 0, 0};
-	// キャラクターの移動速度
-	const float kCharacterSpeed = 0.2f;
-
-	if (input->PushKey(DIK_LEFT)) {
-		move.x -= kCharacterSpeed;
-	} 
-	if (input->PushKey(DIK_RIGHT)) {
-		move.x += kCharacterSpeed;
-	}
-
-	if (input->PushKey(DIK_UP)) {
-		move.y += kCharacterSpeed;
-	} 
-	if (input->PushKey(DIK_DOWN)) {
-		move.y -= kCharacterSpeed;
-	}
-
-	m_worldTransform.translation_+= move;
-
-
-	m_worldTransform.matWorld_ = MakeAffineMatrix(
-	    m_worldTransform.scale_, m_worldTransform.rotation_, m_worldTransform.translation_);
-
 	m_worldTransform.TransferMatrix();
 
-	Copy(m_worldTransform.translation_, imguiPos);
-
-	ImGui::Begin("Player");
-	ImGui::SetWindowPos({0, 0});
-	ImGui::SetWindowSize({300, 100});
-	ImGui::InputFloat3("Position", imguiPos);
-	ImGui::End();
 }
 
 void Player::Draw(const ViewProjection& viewProjection) {
